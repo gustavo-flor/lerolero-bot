39018a15adc49b109fcd4e15fc527f9da4cc25b4
ogustaflor <ogustaflor@gmail.com>
2021-01-24 12:15:01 +0000

    :recycle: Removido Bearer Token desnecessário

--- src/services/twitter.ts
+++ src/services/twitter.ts
@@ -3,7 +3,6 @@ import Twitter from 'twitter';
 import lerolero from './lerolero';
 
 import { 
-  TWITTER_BEARER_TOKEN,
   TWITTER_CONSUMER_KEY, 
   TWITTER_CONSUMER_SECRET, 
   TWITTER_ACCESS_TOKEN_KEY, 
@@ -11,11 +10,10 @@ import {
 } from '../environments';
 
 const client = new Twitter({
-  bearer_token: TWITTER_BEARER_TOKEN,
   consumer_key: TWITTER_CONSUMER_KEY,
   consumer_secret: TWITTER_CONSUMER_SECRET,
   access_token_key: TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const tweetar = async () => {
